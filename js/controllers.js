e6a76d2b20abbcdf867a8f81f963417f64e6d673
nergal-perm <nergal@psu.ru>
2016-04-28 11:54:58 +0500

    Throwed away DB integration, aligned development with AngularJS course on Coursera.org

--- js/controllers.js
+++ js/controllers.js
@@ -4,77 +4,17 @@
 
 angular.module('RapidLog').controller('DailyCtrl', DailyCtrl); 
 
-function DailyCtrl( dailyService, weatherService, $mdSidenav, $q, $scope) {
+function DailyCtrl( fakeDataService, $mdDialog ) {
 	var self = this;
-	
-	// fields
-	self.selectedDay = {};
-	self.selectedModule = null;
-	self.selectedDate = new Date();
-	self.dayRecords = [];
-	
-	// methods
-	self.hasDayRecord = hasDayRecord;
-	self.selectModule = selectModule;
-	self.loadDay = loadDay;
-	self.getForecast = getForecast;
+	this.dayRecords = fakeDataService.dayRecords;
+	this.selectedDay = this.dayRecords[0];
+	this.selDate = Date.parse(this.selectedDay._id);
+	this.selectedSection = 'overview';
+	this.setSection = function(sectionType) {
+		this.selectedSection = sectionType;
+	}
 
-	dailyService.getDayRecordsDates(function(result) {
-		self.dayRecords = [];
-		for (var r in result.rows) {
-			self.dayRecords.push(result.rows[r].id);
-		}
-		console.log(JSON.stringify(self.dayRecords));
-	});
-
-	function selectModule(moduleName) {
-		if (moduleName == "weather") {
-			if (!self.selectedDay['weather']) {
-				self.selectedDay['weather'] = [];
-			}
-			getForecast(function(result) {
-				self.selectedDay[moduleName].length = 0;
-				$scope.$apply(function () {
-					for (var r in result) {
-						self.selectedDay[moduleName].push(result[r]);
-					}
-					self.selectedModule = self.selectedDay[moduleName];					
-				})
-			})
-		} else {
-			self.selectedModule = self.selectedDay[moduleName];
-		}
-	}
-	
-	function loadDay() {
-		dailyService.loadDay(self.selectedDate, function(result) {
-			self.selectedDay = result;
-		});
-	}
-	
-	function hasDayRecord(d) {
-    var dateAsId = d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2); 		
-		return self.dayRecords.indexOf(dateAsId) >= 0;
-	}
-	
-	function getForecast(callback) {
-		if (self.selectedDay['weather'].length > 0) {
-			callback(self.selectedDay['weather']);
-		} else {
-			weatherService.getForecast(function(err, result) {
-				if (err) {
-					callback([{
-						bullet: "err",
-						short: err
-					}]);
-				} else {
-					callback(result);
-				}
-			});			
-		}
-	}
-}
+} // DailyCtrl
 
 })();
-
 
--- js/services.js
+++ js/services.js
@@ -1,75 +1,49 @@
 (function(){
   'use strict';
 
-  var localDB = new PouchDB('local');
-  var remoteDB = new PouchDB('http://localhost:5984/bullet_journal'); 
-
   angular.module('RapidLog')
-  .service('dailyService', dailyService);
-  
-  function dailyService() {
-
-    this.loadDay = function(d, callback) {
-    // Форматирование даты: http://stackoverflow.com/a/30272803/4180503
-    var dateAsId = d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2); 
-    
-    localDB.get(dateAsId).then(function(doc) {
-      callback(doc);
-    })
-  }
-  
-  this.getDayRecordsDates = function(callback) {
-    localDB.query(function (doc, emit) {
-      emit(doc._id);
-    }).then(function(result) {
-      callback(result);
-    }).catch(function(err) {
-      console.log(err);
-    }); 
-  } 
-}
-
-angular.module('Weather')
-.service('weatherService', weatherService);
-
-function weatherService() {
-  var request = require('request'),
-  conf = require('nconf');
-
-  conf.argv()
-  .env()
-  .file({ file: 'config.json'});
-
-  this.getForecast = function(callback) {
-    var api_key = conf.get('weatherAPI');
-    //process.env.http_proxy = conf.get('workProxy');
-    console.log("Weather api key: " + api_key);
-    request.get({
-      uri: 'http://api.wunderground.com/api/' + api_key + '/forecast/lang:RU/q/uspp.json',
-      headers: {'Accept': 'application/json'}
-    }, function(err, res, body) {
-      if (err) {
-        callback(err);  
-      } else {
-        console.log(body);
-        var forecast = JSON.parse(body);
-        var fc_day = forecast.forecast.txt_forecast.forecastday[0];
-        var fc_night = forecast.forecast.txt_forecast.forecastday[1];
-        var result = [];
-        result.push({ 
-          bullet: fc_day.title,
-          short: fc_day.fcttext_metric
-        });
-        result.push({
-          bullet: fc_night.title,
-          short: fc_night.fcttext_metric
-        });
-        console.log('Got weather data, transferring to callback');
-        callback(null, result);
-      }
-    }); 
-  }
-}
+    .service('fakeDataService', dataService);
 
 
+  function dataService() {
+    this.dayRecords = [{
+      _id: "2016-04-27",
+      sections: [
+      {
+        type: "overview",
+        name: "Обзор"
+      }],
+      rows: [
+      {
+        type: "overview",
+        bullet: "Д",
+        short: "Значительная облачность. Повышение 12C. Ветер ВСВ от 10 до 15 км/ч."
+      },
+      {
+        type: "overview",
+        bullet: "Н",
+        short: "Переменная облачность. Понижение 0C. Ветер СВ от 10 до 15 км/ч."
+      }]
+    }];
+
+    this.availableSections = [
+    {
+      type: "plan",
+      name: "Задачи"
+    },
+    {
+      type: "food",
+      name: "Питание"
+    },
+    {
+      type: "diary",
+      name: "Дневник"
+    },
+    {
+      type: "chronodex",
+      name: "Хронодекс"
+    }
+    ]
+
+  };
 })();
