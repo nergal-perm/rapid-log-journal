14d129c3633f4134da52711dd5c1798f1c40d38a
nergal-perm <nergal@psu.ru>
2016-04-28 15:44:18 +0500

    Implemented new section and new row creation for a current day

--- js/controllers.js
+++ js/controllers.js
@@ -4,14 +4,38 @@
 
 angular.module('RapidLog').controller('DailyCtrl', DailyCtrl); 
 
-function DailyCtrl( fakeDataService, $mdDialog ) {
+function DailyCtrl( fakeDataService, $mdDialog, $scope ) {
 	var self = this;
+	
 	this.dayRecords = fakeDataService.dayRecords;
 	this.selectedDay = this.dayRecords[0];
 	this.selDate = Date.parse(this.selectedDay._id);
 	this.selectedSection = 'overview';
 	this.setSection = function(sectionType) {
 		this.selectedSection = sectionType;
+	}
+
+	this.availableSections = fakeDataService.availableSections;
+	this.sectionToAdd = '';
+
+	this.addSection = function() {
+		var removeIndex = this.availableSections.map(function(item) {
+			return item.type;
+		}).indexOf(this.sectionToAdd);
+
+		this.selectedDay.sections.push(~removeIndex && this.availableSections.splice(removeIndex, 1)[0]);
+		this.setSection(this.sectionToAdd);
+	}
+
+	this.addRecord = function() {
+		var record = {
+			type: this.selectedSection,
+			bullet: $scope.bullet,
+			short: $scope.short
+		};
+		this.selectedDay.rows.push(record);
+		$scope.bullet = '';
+		$scope.short = '';
 	}
 
 } // DailyCtrl
